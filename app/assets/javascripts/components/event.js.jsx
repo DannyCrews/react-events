a00c33f26de37771b5a66c996c019df945d8106c
dannycrews <dancrews@me.com>
2016-12-21 17:56:15 -0500

    Convert Event into a stateless functional component

--- app/assets/javascripts/components/event.js.jsx
+++ app/assets/javascripts/components/event.js.jsx
@@ -1,21 +1,19 @@
-var Event = React.createClass({
-  proptypes: {
+const Event = (props) => {
+  Event.proptypes = {
     name: React.PropTypes.string,
     event_date: React.PropTypes.string,
     place: React.PropTypes.string,
     description: React.PropTypes.string
-  },
-
-  render() {
-    var event = this.props.event;
-
-    return(
-      <tr>
-        <td>{event.name}</td>
-        <td>{event.event_date}</td>
-        <td>{event.place}</td>
-        <td>{event.description}</td>
-      </tr>
-    )
   }
-});
+
+  const event = props.event;
+
+  return(
+    <tr>
+      <td>{event.name}</td>
+      <td>{event.event_date}</td>
+      <td>{event.place}</td>
+      <td>{event.description}</td>
+    </tr>
+  )
+}
