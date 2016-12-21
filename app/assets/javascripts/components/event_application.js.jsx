325fa07e0425c8263bde8e52dfcdb86c912c4780
dannycrews <dancrews@me.com>
2016-12-21 15:14:32 -0500

    Add EventApplication component

---
+++ app/assets/javascripts/components/event_application.js.jsx
@@ -0,0 +1,16 @@
+var EventApplication = React.createClass({
+  render() {
+    return(
+      <div className="container">
+        <div className="jumbotron">
+          <h1>React Event Application</h1>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+
+          </div>
+        </div>
+      </div>
+    )
+  }
+});
